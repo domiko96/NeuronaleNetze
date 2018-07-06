0c20a724537925d8fb2d061acaed91d1f1b13443
domiko96 <domiko96@gmail.com>
2018-07-06 18:37:15 +0200

    final code

---
+++ src/Run.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from data.mnist_seven import MNISTSeven
+from model.stupid_recognizer import StupidRecognizer
+from model.perceptron import Perceptron
+from model.logistic_regression import LogisticRegression
+from model.mlp import MultilayerPerceptron
+
+from report.evaluator import Evaluator
+from report.performance_plot import PerformancePlot
+
+
+def main():
+    data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000,
+                                                    oneHot=False)
+    myMLPClassifier = MultilayerPerceptron(data.trainingSet,
+                                        data.validationSet,
+                                        data.testSet,
+                                        learningRate=0.05,
+                                        epochs=20)
+    #no more changes after 20 epochs
+
+    #Removed old stuff
+                                        
+    
+    # Report the result #
+    print("=========================")
+    evaluator = Evaluator()                                        
+
+    # Train the classifiers
+    print("=========================")
+    print("Training..")
+
+    print("\nMLP has been training..")
+    myMLPClassifier.train()
+    print("Done..")
+
+    # Do the recognizer
+    # Explicitly specify the test set to be evaluated
+    mlpPred = myMLPClassifier.evaluate()
+    
+    # Report the result
+    print("=========================")
+    evaluator = Evaluator()
+
+    print("\nResult of the MLP recognizer:")
+    #evaluator.printComparison(data.testSet, lrPred)    
+    evaluator.printAccuracy(data.testSet, mlpPred)
+    
+    # Draw
+    plot = PerformancePlot("MLP validation")
+    plot.draw_performance_epoch(myMLPClassifier.performances,
+                                myMLPClassifier.epochs)
+    
+    
+if __name__ == '__main__':
+    main()
